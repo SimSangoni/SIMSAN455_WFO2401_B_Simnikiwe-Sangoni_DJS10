e3ed977de55aa8ddde19f7bb78472e02698fc656
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-06-10 22:09:43 +0200

    fetching data to see what is inside

--- src/BlogPosts.jsx
+++ src/BlogPosts.jsx
@@ -2,6 +2,11 @@ import React from "react"
 
 export default function BlogPosts() {
 
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => console.log(data[0]))
+    .catch(error => console.error('Error:', error));
+
   return (
     <>
       
@@ -10,5 +15,13 @@ export default function BlogPosts() {
     </>
   )
 }
+
+// {
+//     userId: 1, 
+//     id: 1, 
+//     title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', 
+//     body: 'quia et suscipit\nsuscipit recusandae consequuntur 
+//     …strum rerum est autem sunt rem eveniet architecto'
+// }
 
  
